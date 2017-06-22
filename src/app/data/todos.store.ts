fa086f96f039bc678b47fc44361972bd0e226eb1
Dmitriy Mihaylenko <dmitriy.mihaylichenko@valor-software.com>
2017-06-22 09:00:27 +0300

    Update todos.store.ts

--- src/app/data/todos.store.ts
+++ src/app/data/todos.store.ts
@@ -1,7 +1,4 @@
-import { combineReducers } from '@ngrx/store';
-
 import { Todo } from './todo';
-import { todosReducer } from './todos.reducer';
 
 export interface TodosStore {
     todos: Todo[];
